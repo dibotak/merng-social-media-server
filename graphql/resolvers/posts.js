24b5fe74054bff04c4f9801c96cc2d617674426f
Adib Faqih <foezst@gmail.com>
2020-10-23 18:01:14 +0700

    Finishing the server
    - created comment, like, and count for each.
    - using subscription to know the newest post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -31,6 +31,10 @@ module.exports = {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
+      if (body.trim() === '') {
+        throw new Error('Post must be not empty');
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -39,6 +43,10 @@ module.exports = {
       });
 
       const post = await newPost.save();
+
+      context.pubsub.publish('NEW_POST', {
+        newPost: post
+      });
 
       return post;
     },
@@ -56,6 +64,32 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find(like => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError('Post not found');
+      }
+    }
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
     }
   }
 };
