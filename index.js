b48ef9e3b9ecfc4647b90342a10b1700aeb0a522
Adib Faqih <foezst@gmail.com>
2020-10-29 16:46:29 +0700

    typo on process.env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MONGODB } = require('./config');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
-const PORT = proces.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const pubsub = new PubSub();
 
