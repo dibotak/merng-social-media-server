e1d6fcbee412b09f97281ac15d7b08850d99bc9c
Adib Faqih <foezst@gmail.com>
2020-10-29 15:30:19 +0700

    created new repo for client

--- client/src/util/MyPopup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Popup } from 'semantic-ui-react';
-
-function MyPopup({ content, children }) {
-  return <Popup content={content} inverted trigger={children} />
-}
-
-export default MyPopup;
